7196b9ab218e3c252ac6d32a42380cad21324c8d
sonson-arch <sondrhso@stud.ntnu.no>
2024-09-25 22:18:39 +0200

    #

--- client/test/task-components.test.tsx
+++ client/test/task-components.test.tsx
@@ -1,23 +1,40 @@
 import * as React from 'react';
-import { TaskList, TaskNew } from '../src/task-components';
+import { TaskList, TaskNew, TaskDetails } from '../src/task-components';
 import { shallow } from 'enzyme';
-import { Form, Button } from '../src/widgets';
+import { Form, Button, Column, Row, Card } from '../src/widgets';
 import { NavLink } from 'react-router-dom';
+
+//npm test -- -i task-components.test.tsx
 
 jest.mock('../src/task-service', () => {
   class TaskService {
     getAll() {
       return Promise.resolve([
-        { id: 1, title: 'Les leksjon', done: false },
-        { id: 2, title: 'Møt opp på forelesning', done: false },
-        { id: 3, title: 'Gjør øving', done: false },
+        {
+          id: 1,
+          title: 'Les leksjon',
+          description: 'Notater fra leksjon den 25.09.24',
+          done: false,
+        },
+        { id: 2, title: 'Møt opp på forelesning', description: '', done: false },
+        { id: 3, title: 'Gjør øving', description: '', done: false },
       ]);
+    }
+
+    get() {
+      return Promise.resolve({
+        id: 1,
+        title: 'Les leksjon',
+        description: 'Notater fra leksjon den 25.09.24',
+        done: false,
+      });
     }
 
     create() {
       return Promise.resolve(4); // Same as: return new Promise((resolve) => resolve(4));
     }
   }
+
   return new TaskService();
 });
 
@@ -32,7 +49,7 @@ describe('Task component tests', () => {
           <NavLink to="/tasks/1">Les leksjon</NavLink>,
           <NavLink to="/tasks/2">Møt opp på forelesning</NavLink>,
           <NavLink to="/tasks/3">Gjør øving</NavLink>,
-        ])
+        ]),
       ).toEqual(true);
       done();
     });
@@ -51,5 +68,40 @@ describe('Task component tests', () => {
       expect(location.hash).toEqual('#/tasks/4');
       done();
     });
+  });
+});
+
+describe('TaskDetails tests', () => {
+  test('TaskDetails renders correctly', (done) => {
+    const wrapper = shallow(<TaskDetails match={{ params: { id: 1 } }} />);
+
+    setTimeout(() => {
+      wrapper.update(); // Ensure the component is updated
+
+      console.log(wrapper.debug()); // Debug the rendered output
+
+      expect(
+        wrapper.containsAllMatchingElements([
+          <Card title="Task">
+            <Row>
+              <Column width={2}>Title:</Column>
+              <Column>Les leksjon</Column>
+            </Row>
+            <Row>
+              <Column width={2}>Description:</Column>
+              <Column>Notater fra leksjon den 25.09.24</Column>
+            </Row>
+            <Row>
+              <Column width={2}>Done:</Column>
+              <Column>
+                <Form.Checkbox checked={false} />
+              </Column>
+            </Row>
+          </Card>,
+          <Button.Success>Edit</Button.Success>,
+        ])
+      ).toEqual(true);
+      done();
+    }, 0);
   });
 });
