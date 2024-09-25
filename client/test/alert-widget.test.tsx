eda202772fdab80378ee97a94c7b89fc5c5a8e93
sonson-arch <sondrhso@stud.ntnu.no>
2024-09-25 21:38:04 +0200

    #

--- client/test/alert-widget.test.tsx
+++ client/test/alert-widget.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Alert } from '../src/widgets';
 import { shallow } from 'enzyme';
+import { wrap } from '@cfaester/enzyme-adapter-react-18/dist/enzyme-adapter-utils';
 
 //npm test -- -i alert-widget.test.tsx
 
@@ -11,41 +12,65 @@ describe('Alert tests', () => {
     expect(wrapper.matchesElement(<div></div>)).toEqual(true);
   });
 
-  test('Show alert message', (done) => {
+  test('Show 3 alert messages', (done) => {
     const wrapper = shallow(<Alert />);
 
     Alert.danger('test');
+    Alert.danger('test2');
+    Alert.danger('test3');
+
+    // Force re-render
+    wrapper.update();
 
     // Wait for events to complete
     setTimeout(() => {
+      wrapper.update(); // Ensure the component is updated
+
+      console.log(wrapper.debug()); // Debug the rendered output
+
       expect(
         wrapper.matchesElement(
           <div>
-            <div>
+            <div className="alert alert-dismissible alert-danger" role="alert">
               test
-              <button />
+              <button type="button" className="btn-close btn-sm" />
+            </div>
+            <div className="alert alert-dismissible alert-danger" role="alert">
+              test2
+              <button type="button" className="btn-close btn-sm" />
+            </div>
+            <div className="alert alert-dismissible alert-danger" role="alert">
+              test3
+              <button type="button" className="btn-close btn-sm" />
             </div>
           </div>
         )
       ).toEqual(true);
 
       done();
-    });
+    }, 0);
   });
 
-  test('Close alert message', (done) => {
+  test('Close alert message for second one', (done) => {
     const wrapper = shallow(<Alert />);
 
-    Alert.danger('test');
+    Alert.danger('test2');
+
+    // Force re-render
+    wrapper.update();
 
     // Wait for events to complete
     setTimeout(() => {
+      wrapper.update(); // Ensure the component is updated
+
+      console.log(wrapper.debug()); // Debug the rendered output
+
       expect(
         wrapper.matchesElement(
           <div>
-            <div>
-              test
-              <button />
+            <div className="alert alert-dismissible alert-danger" role="alert">
+              test2
+              <button type="button" className="btn-close btn-sm" />
             </div>
           </div>
         )
@@ -53,9 +78,13 @@ describe('Alert tests', () => {
 
       wrapper.find('button.btn-close').simulate('click');
 
-      expect(wrapper.matchesElement(<div></div>)).toEqual(true);
-
-      done();
+      setTimeout(() => {
+        wrapper.update(); // Ensure the component is updated
+        expect(wrapper.matchesElement(<div></div>)).toEqual(true);
+        done();
+      }, 0);
     });
   });
 });
+
+
